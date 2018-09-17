c51ffecd8b8d4e628f6feded1a2906f4fda76c09
Владислав Царапкин <scratchor@mail.ru>
2018-09-17 21:40:12 +0300

    task 3 ready

--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+    this.func = function compareNumeric (a, b) {
+      return a - b;
+    }
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
   }
 
   sort(indices) {
     // your implementation
+   let arrSort = [];
+    for (let i=0; i<indices.length; i++) {
+      arrSort.push(this.arr[indices[i]]);      
+    }
+    arrSort.sort(this.func);
+    indices.sort(function compareNumeric(a, b) {
+      return a - b;
+    });
+    for(let i=0; i<arrSort.length; i++) {
+      this.arr.splice(indices[i], 1, arrSort[i]);
+    }
+
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.func = compareFunction;
   }
 }
 
